de57986a484b753d317efee06fe7e912317800f8
YefferSime <simehuaranccay@gmail.com>
2024-03-24 22:35:54 -0500

    Ayudame please

---
+++ apli03.js
@@ -0,0 +1,18 @@
+const http =require('http')
+fs=require('fs');
+
+var html=fs.readFileSync('./index.html');
+
+const host='127.0.0.1'
+const port=3000
+
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write(html)
+    res.end()
+})
+
+server.listen(port,host,()=>{
+    console.log("servidor funcionando en",host,port)
+});
+
